1393ab0743c424c86ec5cf8f96fa87be3f9357c8
hyrious <hyrious@outlook.com>
2019-12-28 14:58:04 +0800

    update

--- main.rb
+++ main.rb
@@ -2,46 +2,35 @@
 require_relative 'lib/ctrl_b'
 require_relative 'lib/helper'
 
-bg = spr [Graphics.width, Graphics.height]
-ssr bg, %{
-  res = 0.5f;
-}
-
-def sample_sprite char, opt={}
-  spr [100, 100], opt do |s, b|
-    b.font.size = 72
-    b.font.color = Color.new(0x66, 0xcc, 0xff)
-    b.font.outline = false
-    b.draw_text b.rect, char, 1
-    s.ox, s.oy = s.width / 2, s.height / 2
-    s.x, s.y = Graphics.width / 2, Graphics.height / 2
-  end
+def make c
+  s = spr([32, 32]) { |_, b| b.draw_text b.rect, c, 1 }
+  s.ox, s.oy = s.width / 2, s.height / 2
+  s
 end
 
-v = Viewport.new
-s = sample_sprite '⑨', viewport: v
-s.x -= 18
-t = sample_sprite '葱', viewport: v
-t.x += 18
+def update s, t
+  s.x = 2 * t * Math.sin(2.0 * Graphics.frame_count / t)
+  s.y = -2 * t * Math.cos(2.0 * Graphics.frame_count / t)
+  s.angle += t
+end
 
-# float4 res, color;
-# float2 texcoord;
-ssr v, %{
-  static const float PIXEL_SIZE = 4.0;
-}, %q[
-  float2 one = float2(sampWidth, sampHeight);
-  float2 sqa = one * PIXEL_SIZE;
-  texcoord = sqa * floor(texcoord / sqa);
-  for (int i = 0; i < int(PIXEL_SIZE); ++i)
-    for (int j = 0; j < int(PIXEL_SIZE); ++j)
-      res += tex2D(spriteSampler, texcoord + one * float2(i, j));
-  res /= pow(PIXEL_SIZE, 2.0);
-]
+sun = make '★'
+earth = make '○'
+luna = make '·'
 
+sun.add_child earth
+earth.add_child luna
+
+Graphics.vsync = false
+Graphics.frame_rate = 250
 Graphics.background_exec = true
 rgss_main {
   loop {
     Mouse.update
+    sun.x, sun.y = Mouse.x, Mouse.y
+    sun.angle -= 1
+    update earth, 40
+    update luna, 10
     Graphics.update
   }
 }
---
+++ reset.rb
@@ -0,0 +1,28 @@
+class Object
+  def try sym, *args
+    __send__ sym, *args if respond_to? sym
+  end
+end
+
+class String
+  def strip_heredoc
+    indent = scan(/^[ \t]*(?=\S)/).min.try(:size) || 0
+    gsub(/^[ \t]{#{indent}}/, '')
+  end
+end
+
+save_data [
+  [rand(32768), 'Main', Zlib::Deflate.deflate(<<-RUBY.strip_heredoc)]
+    require 'main.rb'
+  RUBY
+], 'Scripts.rvdata2'
+
+open 'Game.ini', 'w' do |f|
+  f.puts <<-INI.strip_heredoc
+    [Game]
+    RTP=
+    Title=Project1
+    Library=RGSS301.dll
+    Scripts=Scripts.rvdata2
+  INI
+end
